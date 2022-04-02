12522345c01b843277000aa81e2184941b91f24c
Benedict <benedictryan80@gmail.com>
2022-04-03 00:23:02 +0700

    edit app.js title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 
-				<code>GitHub Actions with Netlify</code>
+				<code>GitHub Actions with Netlify and React</code>
 
 				<a
 					className="App-link"
